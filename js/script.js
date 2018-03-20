15636e4a7b67825e1e2f975554ef247ac3464f0c
Tomasz Chowaniec <35344311+TC-Code@users.noreply.github.com>
2018-03-20 21:32:59 +0100

    Update script.js - two loops

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 function drawTree(a) {
-
-    for (var i = 1; i <= a; i++) {
-        var star = '';
-        for (var j = 1; j <= a - i; j++) {
-            star += ' ';
-        }
-		for (var k = 1; k <= (i * 2 - 1); k++) {
-            star += '*';
-        }
-        console.log(star);
+  var star = '';
+  for (var i = 0; i < a; i++) {
+    star += "* ";
+    var space = '';
+    for (var j = a; j > i; j--) {
+      space += ' ';
     }
+    console.log(space + star);
+  }
 }
-drawTree(10);
+
+drawTree(4);
